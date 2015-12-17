7e1376eee6b653b944e321529e631759ffe0fbe8
efuet eric <efuet eric>
2015-12-17 22:17:27 +0100

    first commit

---
+++ AccountClass/MyAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace AccountClass
+{
+    class MyAccount
+    {
+        public double currentBalance = 500;
+        public double amount = 0;
+
+        public String deposit(double amount)
+        {
+            Contract.Requires(amount > 0);
+            Contract.Ensures(Contract.OldValue(currentBalance) + amount == currentBalance);
+            currentBalance += amount;
+            return "Your Current Balance is: " + currentBalance;
+        }
+
+
+        public String withdraw(double amount)
+        {
+            Contract.Requires(currentBalance > amount);
+            Contract.Requires(amount > 0);
+            Contract.Requires(currentBalance >= amount);
+            Contract.Ensures(Contract.OldValue(currentBalance).Equals(amount + currentBalance));
+            this.amount = amount;
+            currentBalance = currentBalance - amount;
+
+            return "Balance: " + currentBalance;
+        }
+        
+        
+    }
+}
---
+++ AccountClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyAccount account = new MyAccount();
+            Console.WriteLine("100 Deposited: " + account.deposit(50));
+            Console.WriteLine("50 withdrawed: " + account.withdraw(75));
+            Console.ReadLine();
+        }
+
+
+    }
+}
